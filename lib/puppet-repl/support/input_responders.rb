eab6b41bb099ced411c05a166bd08eeb748a7cf1
Corey Osman <corey@nwops.io>
2016-05-08 22:02:31 -0700

    Fixes #14 - add functionality to set node object from remote source
      * adds new error handling classes
      * now bubbles error upward after parsing error

--- lib/puppet-repl/support/input_responders.rb
+++ lib/puppet-repl/support/input_responders.rb
@@ -3,7 +3,7 @@ module PuppetRepl
     module InputResponders
 
       def static_responder_list
-        ["exit", "functions", "vars", "krt", "facts",
+        ["exit", "functions", "classification","vars", "krt", "facts",
          "resources", "classes", "play","reset", "help"
         ]
       end
@@ -12,8 +12,38 @@ module PuppetRepl
         PuppetRepl::Cli.print_repl_desc
       end
 
+      def handle_set(input)
+        output = ''
+        args = input.split(' ')
+        args.shift # throw away the set
+        case args.shift
+        when /node/
+          if name = args.shift
+            reset
+            set_node_from_name(name)
+          else
+            out_buffer.puts "Must supply a valid node name"
+          end
+        when /loglevel/
+          if level = args.shift
+            @log_level = level
+            set_log_level(level)
+            output = "loglevel #{Puppet::Util::Log.level} is set"
+          end
+        end
+        output
+      end
+
+      def node_facts
+        if node.facts
+          node.facts.values
+        else
+          node.parameters
+        end
+      end
+
       def facts(args=[])
-        variables = node.facts.values
+        variables = node_facts
         variables.ai({:sort_keys => true, :indent => -1})
       end
 
@@ -23,7 +53,7 @@ module PuppetRepl
 
       def vars(args=[])
         # remove duplicate variables that are also in the facts hash
-        variables = scope.to_hash.delete_if {| key, value | node.facts.values.key?(key) }
+        variables = scope.to_hash.delete_if {| key, value | node_facts.key?(key) }
         variables['facts'] = 'removed by the puppet-repl' if variables.key?('facts')
         output = "Facts were removed for easier viewing".ai + "\n"
         output += variables.ai({:sort_keys => true, :indent => -1})
@@ -59,6 +89,10 @@ module PuppetRepl
         config = {}
         config[:play] = args.first
         play_back(config)
+      end
+
+      def classification
+        node.classes.ai
       end
 
       def resources(args=[])
--- lib/puppet-repl/support/node.rb
+++ lib/puppet-repl/support/node.rb
@@ -1,3 +1,5 @@
+require 'puppet/indirector/node/rest'
+
 module PuppetRepl
   module Support
     module Node
@@ -14,6 +16,20 @@ module PuppetRepl
       # @return [node] puppet node object
       def node
         @node ||= create_node
+      end
+
+      def get_remote_node(name)
+        indirection = Puppet::Indirector::Indirection.instance(:node)
+        indirection.terminus_class = 'rest'
+        indirection.find(name, :environment => puppet_environment)
+      end
+
+      def set_node_from_name(name)
+        out_buffer.puts ("Fetching node #{name}")
+        node_object = get_remote_node(name)
+        # remove trusted data as it will later get populated during compilation
+        node_object.trusted_data = node_object.parameters.delete('trusted')
+        set_node(node_object)
       end
 
       def set_node(value)
