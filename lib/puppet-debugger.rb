9b9884c14f83fc91cdfc55a90dfe87bf369d3b62
Corey Osman <corey@nwops.io>
2017-05-23 19:47:39 -0700

    Adds bold text to commands output

--- lib/puppet-debugger.rb
+++ lib/puppet-debugger.rb
@@ -14,6 +14,15 @@ class String
     "\033[31m#{self}\033[0m"
   end
 
+  def bold;
+    "\033[1m#{self}\033[22m"
+  end
+
+
+  def black
+    "\033[30m#{self}\033[0m"
+  end
+
   def green
     "\033[32m#{self}\033[0m"
   end
