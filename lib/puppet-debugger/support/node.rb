dab4b80b68cbcc90d5cb76765d76fc4c70dee0a9
Corey Osman <corey@nwops.io>
2018-09-24 18:32:04 -0700

    Fix for puppet 6 and trusted server facts

--- lib/puppet-debugger/support/node.rb
+++ lib/puppet-debugger/support/node.rb
@@ -8,7 +8,7 @@ module PuppetDebugger
       # creates a node object using defaults or gets the remote node
       # object if the remote_node_name is defined
       def create_node
-        Puppet[:trusted_server_facts] = true if Puppet.version.to_f >= 4.1
+        Puppet[:trusted_server_facts] = true if Puppet.version.to_f >= 4.1 && Puppet.version.to_f < 6.0
 
         if remote_node_name
           # refetch
